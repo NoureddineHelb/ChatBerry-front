c4de92eb72b3cc32ea86f3dfaef331ec72c4bf29
NoureddineHelb <nbozanih@helb-prigogine.be>
2024-02-05 14:43:11 +0100

    Home page start and selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {CommonModule} from "@angular/common";
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {ChatType} from "../interfaces/chat.type";
+import {MatError, MatFormField, MatHint, MatLabel} from "@angular/material/form-field";
+import {MatOption} from "@angular/material/autocomplete";
+import {MatSelect} from "@angular/material/select";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, FormsModule, MatError, MatFormField, MatHint, MatLabel, MatOption, MatSelect, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ChatBerryFront';
+  title = 'ChatBerry';
+
+  //chosse type section
+  chatTypeControl = new FormControl<ChatType | null>(null, Validators.required);
+  selectFormControl = new FormControl('', Validators.required);
+  chatTypes: ChatType[] = [
+    { name: 'Assistant', type: 'Helpful assistant' },
+    { name: 'Programmer', type: 'Code Interpreter!' },
+  ];
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from './app.component';
+import {ChatbotComponent} from './chatbot/chatbot.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'app-component', component: AppComponent },
+    { path: 'chat-bot-component', component: ChatbotComponent },
+    //{ path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
