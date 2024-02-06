29f6268b149dd921591b0dfb54a1974b99dd00a6
NoureddineHelb <nbozanih@helb-prigogine.be>
2024-02-06 13:34:11 +0100

    bot in console working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {ChatType} from "../intetrfaces/chat.type";
 import {MatError, MatFormField, MatHint, MatLabel} from "@angular/material/form-field";
 import {MatOption} from "@angular/material/autocomplete";
 import {MatSelect} from "@angular/material/select";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -16,6 +17,16 @@ import {MatSelect} from "@angular/material/select";
 })
 export class AppComponent {
   title = 'ChatBerry';
+  userInput: string = '';
+  messages: { content: string, type: 'sent' | 'received' }[] = [];
+  animationIndexes: number[];
+  backUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.backUrl = 'http://localhost:3000/api/sendMessage';
+    // initialisation avec des veleurs randoms
+    this.animationIndexes = Array.from({ length: 30 }, () => Math.floor(Math.random() * 100));
+  }
 
   //chosse type section
   chatTypeControl = new FormControl<ChatType | null>(null, Validators.required);
@@ -24,4 +35,20 @@ export class AppComponent {
     { name: 'Assistant', type: 'Helpful assistant' },
     { name: 'Programmer', type: 'Code Interpreter!' },
   ];
+
+  // Gestion des messages
+  sendMessage(message: string) {
+    console.log('Message à envoyer : ', message);
+
+    const body = { message: message };
+    const headers = { 'Content-Type': 'application/json' };
+
+    this.http.post(this.backUrl, body, { headers: headers }).subscribe(response => {
+      console.log('Réponse du serveur : ', response);
+      this.messages.push({ content: message, type: 'sent' });
+    }, error => {
+      console.error('Erreur lors de l\'envoi de la requête : ', error);
+    });
+  }
+
 }
